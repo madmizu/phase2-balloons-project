56fe9a394117c6585291137d6204ea480b525cec
Madison Shimizu <mhshimizu@outlook.com>
2021-12-07 06:02:17 -1000

    added 6 floating balloon presents

---
+++ src/components/BalloonGifts.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function BalloonTest () {
+    return (
+        <>
+        <div className="container">
+            <img className='balloon' alt='Red Balloon' src="https://i.ibb.co/5KCVxrH/redballoon.gif">
+            </img>
+            <img className='balloon' alt='Blue Balloon' src="https://i.ibb.co/vD3vc23/blueballoon.gif">
+            </img>
+            <img className='balloon' alt='Purple Balloon' src="https://i.ibb.co/5xCgjFk/purpleballoon.gif">
+            </img>
+            <img className='balloon' alt='Green Balloon' src="https://i.ibb.co/BC7m4YC/greenballoon.gif">
+            </img>
+            <img className='balloon' alt='Yellow Balloon' src="https://i.ibb.co/FzQtrZn/yellowballoon.gif">
+            </img>
+            <img className='balloon' alt='Orange Balloon' src="https://i.ibb.co/3RLTxRL/orangeballoon.gif">
+            </img>
+        </div>
+        </>
+    )
+}
+
+export default BalloonTest;
