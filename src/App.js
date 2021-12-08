7b89918834e5aa0cb48218b023121a69959da979
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-12-08 08:24:10 -0800

    adding ids for balloons and onClick event listener

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import BalloonGifts from './components/BalloonGifts.js';
 
 function App() {
+
+function popBalloon() {
+  
+ }
+
   return (
     <div className="App">
       <header className="App-header">
-        <BalloonGifts />
+        <BalloonGifts popBalloon={popBalloon}/>
       </header>
     </div>
   );
--- src/components/BalloonGifts.js
+++ src/components/BalloonGifts.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-function BalloonTest () {
+function BalloonTest ({popBalloon}) {
     return (
         <>
-        <div className="container">
-            <img className='balloon' alt='Red Balloon' src="https://i.ibb.co/5KCVxrH/redballoon.gif">
+        <div className="container" onClick={() => popBalloon(console.log("popped"))}>
+            <img className='balloon' id='red' alt='Red Balloon' src="https://i.ibb.co/5KCVxrH/redballoon.gif">
             </img>
-            <img className='balloon' alt='Blue Balloon' src="https://i.ibb.co/vD3vc23/blueballoon.gif">
+            <img className='balloon' id='blue' alt='Blue Balloon' src="https://i.ibb.co/vD3vc23/blueballoon.gif">
             </img>
-            <img className='balloon' alt='Purple Balloon' src="https://i.ibb.co/5xCgjFk/purpleballoon.gif">
+            <img className='balloon' id='purple' alt='Purple Balloon' src="https://i.ibb.co/5xCgjFk/purpleballoon.gif">
             </img>
-            <img className='balloon' alt='Green Balloon' src="https://i.ibb.co/BC7m4YC/greenballoon.gif">
+            <img className='balloon' id='green' alt='Green Balloon' src="https://i.ibb.co/BC7m4YC/greenballoon.gif">
             </img>
-            <img className='balloon' alt='Yellow Balloon' src="https://i.ibb.co/FzQtrZn/yellowballoon.gif">
+            <img className='balloon' id='yellow' alt='Yellow Balloon' src="https://i.ibb.co/FzQtrZn/yellowballoon.gif">
             </img>
-            <img className='balloon' alt='Orange Balloon' src="https://i.ibb.co/3RLTxRL/orangeballoon.gif">
+            <img className='balloon' id='orange' alt='Orange Balloon' src="https://i.ibb.co/3RLTxRL/orangeballoon.gif">
             </img>
         </div>
         </>
     )
 }
 
+
+
 export default BalloonTest;
+
+
+//use if statement if clicked === image id of thing clicked
+//balloon is a button, if clicked change to <>
+//overlay effect <div id=overlay> everything else is in css. 
+
