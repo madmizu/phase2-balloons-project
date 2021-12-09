8933c0b98f754d29e35bf5ea655960fa58cd5576
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-12-09 09:54:20 -0800

    added css for Movie Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function MovieCard ({ movie, movieIndex, title, genres, summary, poster}) {
     return (
-        <div className="content">
-            <h2 className="header">{title}</h2>
+        <div className="card-content">
+            <h2 className="title-header">{title}</h2>
             <div className="poster">
-                <img alt={title} src={poster}/>
+                <img className="poster-img" alt={title} src={poster}/>
             </div>
             <p className="overview">{summary}</p>
             <p className="genre">{genres}</p>
